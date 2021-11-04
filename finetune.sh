d9bf74b67b9b9fed298ecae75edc11b7668b55ff
华桦 <hua.hua.hb5@is.naist.jp>
2021-11-04 10:31:24 +0800

    Add files via upload

--- finetune.sh
+++ finetune.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python quant_fast_finetune.py --quant_mode calib --fast_finetune
-python quant_fast_finetune.py  --quant_mode test --subset_len 1 --batch_size=8 --fast_finetune --deploy
+python quant_fast_finetune.py  --quant_mode test --subset_len 1 --batch_size=1 --fast_finetune --deploy
